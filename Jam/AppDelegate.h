ba78d2879d079de543f8ee0b9f81b5637f89187c
5661 <5661@c04-imac11.ait-sydney.net.au>
2018-05-10 15:24:38 +1000

    Initial Commit

---
+++ Jam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jam
+//
+//  Created by 5661 on 10/5/18.
+//  Copyright © 2018 5661. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jam
+//
+//  Created by 5661 on 10/5/18.
+//  Copyright © 2018 5661. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
